993a0dd75fafbefb9be59c069543f6e721e9e62c
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2025-02-24 18:55:07 +0000

    Atualização 2.2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Home(){
-    return <h1>Te amo minha rainha ❤️</h1>
-            <h4>Quer ir comprar açaí ?</h4>
+    return( 
+        <div>
+    <h1>Te amo minha rainha ❤️</h1>;
+    <h4>Quer ir comprar açaí ?</h4>
+    </div>);
 }
 
 export default Home;
