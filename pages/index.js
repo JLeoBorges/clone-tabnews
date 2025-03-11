ee17fe6a029bb4058999b6bdf8d142b5c51c2ede
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2025-03-11 02:46:28 +0000

    Mais um teste

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
     return( 
         <div>
-    <h1>Teste novo Não sei o que ta dando</h1><div>O que está dando de errado ?</div>
+    <h1>Teste novo. Não sei o que ta dando</h1>
     </div>);
     
 }
