111dc352ac62dbcf4f2bf4166e9701909276bd44
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2025-03-11 03:09:49 +0000

    Adiciona o prettier

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
-function Home(){
-    return( 
-        <div>
-    <h1>Teste novo. Não sei o que ta dando</h1>
-    </div>);
-    
+function Home() {
+  return (
+    <div>
+      <h1>Teste novo. Não sei o que ta dando</h1>
+    </div>
+  );
 }
 
-function teste(){
-    console.log('Alô');
+function teste() {
+  console.log("Alô");
+}
+
+function teste2() {
+  console.log("Alô");
 }
 
 export default Home;
