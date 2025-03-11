18cf0b4038e839d9b0b595047ff44de00b591c50
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2025-03-11 02:15:20 +0000

    Mais um teste de commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 function Home(){
     return( 
         <div>
-    <h1>Teste novo</h1>;
+    <h1>Teste novo</h1>
     </div>);
+}
+
+function teste(){
+    console.log('Alô');
 }
 
 export default Home;
